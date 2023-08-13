c14e79dce233dffcf9d2ace12671bd35d4394c04
Abhijit <paulabhijit2607@gmail.com>
2023-08-13 19:33:20 +0530

    Feature - Created the Entry Page.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown2 import markdown
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, name):
+    try:
+        return render(request, "encyclopedia/entry.html", {
+            "title" : name.upper(),
+            "html" : markdown(util.get_entry(name))
+        })
+    except TypeError:
+        return render(request, "encyclopedia/entry.html", {
+            "title" : "ERROR",
+            "html" : "<h1>ERROR!!! PAGE DOES NOT EXIST</h1>"
+        })
