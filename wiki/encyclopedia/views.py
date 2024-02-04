ad656ad88c93e558a1682c331874bf52c6865d69
Abhijit <paulabhijit2607@gmail.com>
2024-02-04 21:23:59 +0530

    Feature: Added functionality to the Create New Page Link which is used to create new entries to the encyclopedia where the user is asked to enter the title of the page and content(in markdown language) in forms and upon clicking the save button the user is redirected the newly created page.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,7 @@
 from random import choice
 
+from django import forms
+
 from django.shortcuts import render
 
 from django import forms
@@ -7,6 +9,11 @@ from django import forms
 from . import util
 
 from markdown2 import markdown
+
+class NewEntry(forms.Form):
+    new_entry_title = forms.CharField(label="TITLE", widget=forms.TextInput(attrs={"style": "width:250px", "placeholder": "Enter the title..."}))
+    new_entry_content = forms.CharField(label="", widget=forms.Textarea(attrs={"style": "width:1000px; height:400px;", "placeholder": "Enter the Markdown Content... "}))
+    new_entry_save_button = forms.CharField(label="", widget=forms.TextInput(attrs={"type": "submit", "value": "Save"}))
 
 
 def convert_md_to_html(title):
@@ -66,3 +73,44 @@ def search(request):
                     "title": "Suggestions",
                     "heading": "Search Result Recommandations"
                 })
+
+
+def new_page(request):
+    if request.method == "POST":
+        form = NewEntry(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["new_entry_title"]
+            content = form.cleaned_data["new_entry_content"]
+
+            file_name = f"D:\CS50 Web Programming PROJECTS\Project 1 WIKI\wiki\entries\{title}.md"
+
+            try:
+                with open(file_name, "x") as file:
+                    file.write(content)
+            except FileExistsError:
+                return render(request, "encyclopedia/entry.html", {
+                    "title": "PageAlreadyExistError",
+                    "html": "<h1> ERROR!!! PAGE ALREADY EXIST WITH THE SAME TITLE </h1>"
+                })
+            
+            content = convert_md_to_html(title)
+            
+            return render(request, "encyclopedia/entry.html", {
+                "title" : title,
+                "html": content
+            })
+
+    else:
+        return render(request, "encyclopedia/new_page.html", {
+            "form": NewEntry()
+        })
+
+
+def random(request):
+    entries = util.list_entries()
+    random_page_title = choice(entries)
+    content = convert_md_to_html(random_page_title)
+    return render(request, "encyclopedia/entry.html",{
+        "title": random_page_title.upper(),
+        "html": content
+    })            
